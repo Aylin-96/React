c53f7a2c55fc292db542e09f8ac279ea65a380f1
Aylin-96 <spacewalker.u@gmail.com>
2020-04-09 10:22:08 +0400

    first commit

--- App.js
+++ App.js
@@ -20,13 +20,35 @@ export default class Chats extends Component {
           </Text>
         </View> 
         <View style={{flex:2, backgroundColor: "darkred"}}>
-          <Text>Open up App.js to start working on your app!</Text>
+          <Text style={styles.baseText}>
+            <Text>Ivan</Text>
+          </Text>
         </View>
-        <View style={{flex:2, backgroundColor: "darksalmon"}}/>
-        <View style={{flex:2, backgroundColor: "darkseagreen"}}/>
-        <View style={{flex:2, backgroundColor: "darkorchid"}}/>
-        <View style={{flex:2, backgroundColor: "mediumaquamarine"}}/>
-        <View style={{flex:2, backgroundColor: "maroon"}}/>
+        <View style={{flex:2, backgroundColor: "darksalmon"}}>
+          <Text style={styles.baseText}>
+            <Text>Nastya</Text>
+          </Text>
+        </View>
+        <View style={{flex:2, backgroundColor: "darkseagreen"}}>
+          <Text style={styles.baseText}>
+            <Text>Tanya</Text>
+          </Text>
+        </View>
+        <View style={{flex:2, backgroundColor: "darkorchid"}}>
+          <Text style={styles.baseText}>
+            <Text>Mariya</Text>
+          </Text>
+        </View>
+        <View style={{flex:2, backgroundColor: "mediumaquamarine"}}>
+          <Text style={styles.baseText}>
+            <Text>Dasha</Text>
+          </Text>
+        </View>
+        <View style={{flex:2, backgroundColor: "maroon"}}>
+          <Text style={styles.baseText}>
+            <Text>Sergey</Text>
+          </Text>
+        </View>
         <View style={{flex:2, backgroundColor: "lightcoral"}}>
           <Text style={styles.baseText}>
             <Text>Igor</Text>
